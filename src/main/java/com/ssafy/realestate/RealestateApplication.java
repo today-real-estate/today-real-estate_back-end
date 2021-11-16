647132a324a9b0607f495af058bd2321a37ab816
jhj960918 <jhj960918@gmail.com>
2021-11-16 23:21:53 +0900

    Set : Initial setting

---
+++ src/main/java/com/ssafy/realestate/RealestateApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.realestate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealestateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealestateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssafy/realestate/RealestateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.realestate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealestateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
