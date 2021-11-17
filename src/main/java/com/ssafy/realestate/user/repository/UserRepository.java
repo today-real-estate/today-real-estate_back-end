1759cadacfa76bcf0491581569adbc20dc95bbe3
jhj960918 <jhj960918@gmail.com>
2021-11-17 18:56:53 +0900

    Modify : @Repository 추가

--- src/main/java/com/ssafy/realestate/user/repository/UserRepository.java
+++ src/main/java/com/ssafy/realestate/user/repository/UserRepository.java
@@ -4,9 +4,10 @@ package com.ssafy.realestate.user.repository;
 import com.ssafy.realestate.user.entity.UserEntity;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
     boolean existsByUserEmail(String email);
