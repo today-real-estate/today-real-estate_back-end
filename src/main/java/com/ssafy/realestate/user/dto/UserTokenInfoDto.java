d3f9fd4959202ad5175b4a29e6def50f2795790d
jhj960918 <jhj960918@gmail.com>
2021-11-17 15:18:14 +0900

    Feat : 로그인시 응답 DTO

---
+++ src/main/java/com/ssafy/realestate/user/dto/UserTokenInfoDto.java
@@ -0,0 +1,14 @@
+package com.ssafy.realestate.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class UserTokenInfoDto {
+    private Long id;
+    private String userName;
+    private String userEmail;
+    private String token;
+
+}
