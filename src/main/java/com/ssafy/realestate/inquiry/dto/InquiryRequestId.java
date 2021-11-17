2e41b0411a434aa038e5624fe529438a3852e984
jhj960918 <jhj960918@gmail.com>
2021-11-18 00:04:17 +0900

    FEAT : 목록 조회

--- src/main/java/com/ssafy/realestate/inquiry/dto/InquiryRequestId.java
+++ src/main/java/com/ssafy/realestate/inquiry/dto/InquiryRequestId.java
@@ -1,2 +1,10 @@
-package com.ssafy.realestate.inquiry.dto;public class InquiryRequestId {
+package com.ssafy.realestate.inquiry.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class InquiryRequestId {
+    private Long userId;
 }
