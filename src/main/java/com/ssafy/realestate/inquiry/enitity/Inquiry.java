a33028fc89fefbe13ca1105336e7d70225450526
jhj960918 <jhj960918@gmail.com>
2021-11-17 22:53:51 +0900

    FEAT : UserEntity mapping

---
+++ src/main/java/com/ssafy/realestate/inquiry/enitity/Inquiry.java
@@ -0,0 +1,39 @@
+package com.ssafy.realestate.inquiry.enitity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.ssafy.realestate.common.BaseTimeEntity;
+import com.ssafy.realestate.user.entity.UserEntity;
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@Builder
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Inquiry extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    @JsonBackReference
+    private UserEntity user;
+
+    @NotBlank(message = "문의 유형이 없습니다.")
+    private String inquiryType;
+
+    @NotBlank(message = "제목을 입력하세요")
+    private String title;
+
+    @NotBlank(message = "내용을 입력하세요")
+    private String content;
+
+    private boolean isComplete;
+
+
+}
