e832347886a74f1b937f5b18190c676810224d79
jhj960918 <jhj960918@gmail.com>
2021-11-18 23:16:59 +0900

    Modify : duplicate nickname ,username API

--- src/main/java/com/ssafy/realestate/user/controller/UserController.java
+++ src/main/java/com/ssafy/realestate/user/controller/UserController.java
@@ -46,10 +46,17 @@ public class UserController {
         return new ResponseEntity(userManagementService.validateDuplicatedEmail(object.get("userEmail")), HttpStatus.OK);
     }
 
-    @PutMapping("/update")
+    @PutMapping("/update/username")
     @PreAuthorize(roles = {"ROLE_ADMIN", "ROLE_USER"})
-    public ResponseEntity<Void> update(@RequestBody @Valid UserUpdateDto userUpdateDto) {
-        userManagementService.update(userUpdateDto);
+    public ResponseEntity<Void> updateName(@RequestBody @Valid UserUpdateDto userUpdateDto) {
+        userManagementService.updateUserName(userUpdateDto);
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @PutMapping("/update/nickname")
+    @PreAuthorize(roles = {"ROLE_ADMIN", "ROLE_USER"})
+    public ResponseEntity<Void> updateNickname(@RequestBody @Valid UpdateNickDto updateNickDto) {
+        userManagementService.updateNickName(updateNickDto);
         return new ResponseEntity(HttpStatus.OK);
     }
 
