5c24897af34854bfa25d4e59799d22f7907f23e6
jhj960918 <jhj960918@gmail.com>
2021-11-21 17:37:48 +0900

    Bug :  Exception 구조 변경

---
+++ src/main/java/com/ssafy/realestate/common/controllerAdvice/ExceptionControllerAdvice.java
@@ -0,0 +1,81 @@
+package com.ssafy.realestate.common.controllerAdvice;
+
+
+import com.ssafy.realestate.common.ErrorResponse;
+import com.ssafy.realestate.inquiry.exception.NoInquiryIdException;
+import com.ssafy.realestate.inquiry.exception.NoInquiryUserIdException;
+import com.ssafy.realestate.user.exception.*;
+import io.jsonwebtoken.JwtException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.nio.file.AccessDeniedException;
+
+@RestControllerAdvice(basePackages = "com.ssafy.realestate")
+@Slf4j
+public class ExceptionControllerAdvice {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> methodArgumentNotValidException(MethodArgumentNotValidException e) {
+        log.error(e.getMessage(), e);
+        return ResponseEntity.badRequest().body(new ErrorResponse(HttpStatus.BAD_REQUEST.value(), e.getBindingResult().getFieldError().getDefaultMessage()));
+    }
+
+    @ExceptionHandler(DuplicatedEmailException.class)
+    public ResponseEntity<ErrorResponse> duplicatedEmailException(DuplicatedEmailException e) {
+        log.error(e.getMessage(), e);
+        return ResponseEntity.badRequest().body(new ErrorResponse(HttpStatus.FORBIDDEN.value(), e.getMessage()));
+    }
+
+    @ExceptionHandler(NoUserFoundException.class)
+    public ResponseEntity<ErrorResponse> noUserFoundException(NoUserFoundException e) {
+        log.error(e.getMessage(), e);
+        return ResponseEntity.badRequest().body(new ErrorResponse(HttpStatus.METHOD_NOT_ALLOWED.value(), e.getMessage()));
+    }
+
+    @ExceptionHandler(NoUserException.class)
+    public ResponseEntity<ErrorResponse> NoUserException(NoUserException e) {
+        log.error(e.getMessage(), e);
+        return ResponseEntity.badRequest().body(new ErrorResponse(HttpStatus.BAD_REQUEST.value(), e.getMessage()));
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ErrorResponse> accessDeniedException(AccessDeniedException e) {
+        log.error(e.getMessage(), e);
+        return ResponseEntity.badRequest().body(new ErrorResponse(HttpStatus.FORBIDDEN.value(), e.getMessage()));
+    }
+
+    @ExceptionHandler(NoTokenException.class)
+    public ResponseEntity<ErrorResponse> noTokenException(NoTokenException e) {
+        log.error(e.getMessage(), e);
+        return ResponseEntity.badRequest().body(new ErrorResponse(HttpStatus.UNAUTHORIZED.value(), e.getMessage()));
+    }
+
+    @ExceptionHandler(JwtException.class)
+    public ResponseEntity<ErrorResponse> jwtException(JwtException e) {
+        log.error(e.getMessage(), e);
+        return ResponseEntity.badRequest().body(new ErrorResponse(HttpStatus.UNAUTHORIZED.value(), e.getMessage()));
+    }
+
+    @ExceptionHandler(FailToAuthenticationException.class)
+    public ResponseEntity<ErrorResponse> failToAuthenticationException(FailToAuthenticationException e) {
+        log.error(e.getMessage(), e);
+        return ResponseEntity.badRequest().body(new ErrorResponse(HttpStatus.UNAUTHORIZED.value(), e.getMessage()));
+    }
+
+    @ExceptionHandler(NoInquiryIdException.class)
+    public ResponseEntity<ErrorResponse> NoInquiryIdException(NoInquiryIdException e) {
+        log.error(e.getMessage(), e);
+        return ResponseEntity.badRequest().body(new ErrorResponse(HttpStatus.BAD_REQUEST.value(), e.getMessage()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> exception(Exception e) {
+        log.error(e.getMessage(), e);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("예상치 못한 에러.");
+    }
+}
