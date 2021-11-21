2178c2ba89c5182c28c13bac695aa0240920f103
jhj960918 <jhj960918@gmail.com>
2021-11-21 21:06:57 +0900

    Remvoe : 1대1문의 유저 Exception

--- src/main/java/com/ssafy/realestate/inquiry/exception/NoInquiryUserIdException.java
+++
@@ -1,7 +0,0 @@
-package com.ssafy.realestate.inquiry.exception;
-
-public class NoInquiryUserIdException extends IllegalArgumentException {
-    public NoInquiryUserIdException() {
-        super("존재하는 사용자 id가 없습니다.");
-    }
-}
