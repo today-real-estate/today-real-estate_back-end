a7a75f22813905f4442f41a2a6c101e8294290d5
jhj960918 <jhj960918@gmail.com>
2021-11-22 14:57:19 +0900

    Feat : answer api

--- src/main/java/com/ssafy/realestate/inquiry/controller/InquiryController.java
+++ src/main/java/com/ssafy/realestate/inquiry/controller/InquiryController.java
@@ -1,13 +1,11 @@
 package com.ssafy.realestate.inquiry.controller;
 
-import com.ssafy.realestate.inquiry.dto.InquiryAnswerDto;
-import com.ssafy.realestate.inquiry.dto.InquiryRequestDto;
-import com.ssafy.realestate.inquiry.dto.InquiryResponseDto;
-import com.ssafy.realestate.inquiry.dto.InquiryUpdateDto;
+import com.ssafy.realestate.inquiry.dto.*;
 import com.ssafy.realestate.inquiry.service.InquiryService;
 import com.ssafy.realestate.user.annotation.PreAuthorize;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.annotations.Delete;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,7 +21,7 @@ public class InquiryController {
 
     @GetMapping
     @PreAuthorize(roles = {"ROLE_ADMIN", "ROLE_USER"})
-    public ResponseEntity<List<InquiryResponseDto>> findByUserId(@RequestParam("userId")Long userId) {
+    public ResponseEntity<List<InquiryResponseDto>> findByUserId(@RequestParam("userId") Long userId) {
         return ResponseEntity.ok(inquiryService.findByUserId(userId));
     }
 
@@ -49,14 +47,27 @@ public class InquiryController {
 
     @DeleteMapping("/delete/{id}")
     @PreAuthorize(roles = {"ROLE_ADMIN", "ROLE_USER"})
-    public ResponseEntity<String> delete(@PathVariable("id")Long id) {
+    public ResponseEntity<String> delete(@PathVariable("id") Long id) {
         inquiryService.deleteById(id);
-        return ResponseEntity.ok("id :"+id+" 삭제완료");
+        return ResponseEntity.ok("id :" + id + " 삭제완료");
     }
 
-    @PostMapping("/answer/{id}")
+    @PostMapping("/answer")
+    @PreAuthorize(roles = {"ROLE_ADMIN"})
     public ResponseEntity<String> inquiryAnswerSave(@RequestBody InquiryAnswerDto InquiryAnswerDto) {
-        inquiryService.answerSave(InquiryAnswerDto);
-        return ResponseEntity.ok("Inquiry ID : "+InquiryAnswerDto.getInquiryId()+" 답변 저장 완료");
+        return ResponseEntity.ok("Inquiry ID : " + inquiryService.answerSave(InquiryAnswerDto) + " 답변 저장 완료");
     }
+
+    @DeleteMapping("/answer/{id}")
+    @PreAuthorize(roles = {"ROLE_ADMIN"})
+    public ResponseEntity<String> inquiryAnswerdelete(@PathVariable("id") Long id) {
+        return ResponseEntity.ok("Inquiry ID : " + inquiryService.answerDelete(id) + " 답변 삭제 완료");
+    }
+
+    @PutMapping("/answer{id}")
+    @PreAuthorize(roles = {"ROLE_ADMIN"})
+    public ResponseEntity<String> inquiryAnswerUpdate(@RequestBody InquiryAnswerUpdateDto inquiryAnswerUpdateDto) {
+        return ResponseEntity.ok("Inquiry ID : " + inquiryService.answerUpdate(inquiryAnswerUpdateDto) + " 답변 수정 완료");
+    }
+
 }
